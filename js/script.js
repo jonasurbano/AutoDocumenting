1a7bd5049b06f2c54e802142bb7bcc0ed56fa47e
Jonás <jonasur@gmail.com>
2016-03-14 00:20:50 +0100

    Rendered objects and sql query

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ var token = null;
 $(document).ready(function () {
     renderVariableFunction = renderVariable;
     renderDataProviderFunction = renderDataProvider;
+    renderObjectsFunction = renderObjects;
+    renderDocumentNameFunction = renderDocumentName;
     
     logOn(userName, password, authenticationType);
 
@@ -11,14 +13,17 @@ $(document).ready(function () {
         log('The token was not received');
         return;
     }
-    
-    queryDataProviders(documentId);
-    
+
+    var document = queryDocument(documentId);
+    renderDocumentName(document);
+
     var varList = queryVariables(documentId);
     variables(documentId, varList);
 
     var providers = queryDataProviders(documentId);
     dataProviders(documentId, providers);
+
+    changeUniverseIdsByNames();
 
     logOff();
 });
@@ -45,6 +50,16 @@ function logonBody(userName, password, authenticationType) {
         + authenticationType + '</attr></attrs>';
 }
 
+function queryDocument(documentId) {
+    var documentRequest = new XMLHttpRequest();
+    documentRequest.open('GET', documentRequestUrl(documentId), false);
+    setHeaders(documentRequest);
+    setToken(documentRequest);
+    documentRequest.send();
+
+    return JSON.parse(documentRequest.responseText).document;
+}
+
 function queryVariables(documentId) {
     var variablesRequest = new XMLHttpRequest();
     variablesRequest.open('GET', variablesRequestUrl(documentId), false);
@@ -68,14 +83,18 @@ function queryDataProviders(documentId) {
 }
 
 function variables(documentId, variables) {
-    $.each(variables, function(i, varr) {
+    $.each(variables, function (i, varr) {
         renderVariableFunction(queryVariable(documentId, varr.id));
     });
 }
 
 function dataProviders(documentId, dataProviders) {
-    $.each(dataProviders, function(i, provider) {
-        renderDataProviderFunction(queryDataProvider(documentId, provider.id));
+    var provider = null;
+    $.each(dataProviders, function (i, provider) {
+        provider = queryDataProvider(documentId, provider.id)
+        renderDataProviderFunction(provider);
+        renderObjects(provider);
+        renderSqlQuery(provider);
     });
 }
 
@@ -101,6 +120,17 @@ function queryDataProvider(documentId, providerId) {
     return parent.dataprovider;
 }
 
+function queryUniverse(documentId, universeId) {
+    var universeRequest = new XMLHttpRequest();
+    var url = universeRequestUrl(documentId, universeId);
+    universeRequest.open('GET', url, false);
+    setHeaders(universeRequest);
+    setToken(universeRequest);
+    universeRequest.send();
+    var parent = JSON.parse(universeRequest.responseText);
+    return parent.universe;
+}
+
 function logOff() {
     var logoffRequest = new XMLHttpRequest();
 
@@ -120,20 +150,46 @@ function setToken(request) {
     request.setRequestHeader('X-SAP-LogonToken', token);
 }
 
+function renderDocumentName(document) {
+    var tableRow = '<tr><td>' + document.name + '</td><td class="description"></td></tr>';
+    $('#document').append(tableRow);
+}
+
 function renderVariable(variable) {
-    var tableRow = '<tr><td>' + variable.name + '</td><td>' 
-        + variable['@qualification'] + '</td><td>' 
+    var tableRow = '<tr><td>' + variable.name + '</td><td>'
+        + variable['@qualification'] + '</td><td>'
         + variable.definition + '</td></tr>';
     $('#variables').append(tableRow);
 }
 
 function renderDataProvider(provider) {
-    var tableRow = '<tr><td>' + provider.name + '</td><td>' + provider.dataSourceId + '</td></tr>';
+    var tableRow = '<tr><td>' + provider.name + '</td><td class="universe">'
+        + provider.dataSourceId + '</td></tr>';
     $('#dataproviders').append(tableRow);
+}
+
+function renderSqlQuery(provider) {
+    var tableRow = '<tr><td>' + provider.name + '</td><td>'
+        + provider.query + '</td></tr>';
+    $('#sqlqueries').append(tableRow);
+}
+
+function renderObjects(provider) {
+    var dictionary = provider.dictionary.expression;
+    var tableRow = '';
+    $.each(dictionary, function (i, object) {
+        tableRow = '<tr><td>' + object.name + '</td><td class="universe">'
+            + provider.dataSourceId + '</td></tr>';
+        $('#objects').append(tableRow);
+    });
 }
 
 function logonRequestUrl() {
     return http + server + ':' + port + '/biprws/logon/long';
+}
+
+function documentRequestUrl(documentId) {
+    return http + server + ':' + port + '/biprws/raylight/v1/documents/' + documentId;
 }
 
 function variablesRequestUrl(documentId) {
@@ -156,6 +212,19 @@ function dataProviderRequestUrl(documentId, providerId) {
         + documentId + '/dataproviders/' + providerId;
 }
 
+function universeRequestUrl(documentId, universeId) {
+    return http + server + ':' + port + '/biprws/raylight/v1/universes/'
+        + universeId;
+}
+
 function logoffRequestUrl() {
     return http + server + ':' + port + '/biprws/logoff';
+}
+
+function changeUniverseIdsByNames() {
+    var universe = null;
+    $('.universe').each(function () {
+        universe = queryUniverse(documentId, $(this).text());
+        $(this).text(universe.name);
+    });
 }
